78a5f0c93841b0b9e51d5d98ae3a901a92251f66
cdownie <corey.downie@gmail.com>
2017-04-04 11:09:47 -0400

    Updated to new pscp.tv url's

--- periscope.js
+++ periscope.js
@@ -1,18 +1,17 @@
 var request = require('request');
 
 function periscope(url, callback) {
-	var id = url.match(/periscope.tv\/w\/(.*)/i)[1];
-	
-	var api = 'https://api.periscope.tv/api/v2/getAccessPublic?broadcast_id=' + id;
-	
+	var id = url.match(/pscp.tv\/w\/(.*)/i)[1];
+  var api = 'https://proxsee.pscp.tv/api/v2/accessVideoPublic?broadcast_id=' + id;
+
 	request(api, function(error, response, body) {
 		if (error || response.statusCode != 200) {
 			callback('Response error: ' + response.statusCode);
 			return;
 		}
-		
+
 		var obj = JSON.parse(body);
-		
+
 		callback(null, obj);
 	});
 }
